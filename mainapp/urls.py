6060047e66d3debc52803fa897552bdaf1d54bb0
Mehrinoz <abdullayevamehrinoz845@gmail.com>
2025-05-28 16:23:53 +0500

    1-commit

---
+++ mainapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from .views import index, test_list
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('kitoblar/', views.book_list, name='books'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('tests/', test_list, name='test_list'),
+    path('tests/<int:test_id>/', views.test_detail, name='test_detail')
+]
+
+
